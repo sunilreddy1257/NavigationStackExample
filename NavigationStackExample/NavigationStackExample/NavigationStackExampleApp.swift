57cf778b90eb00b70adcf338c1b022544465d3b2
sunilreddy1257 <sunilreddy1257@gmail.com>
2023-06-14 16:41:40 +0530

    Xcode Project added

---
+++ NavigationStackExample/NavigationStackExample/NavigationStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExampleApp.swift
+//  NavigationStackExample
+//
+//  Created by Sunil Kumar Reddy Sanepalli on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
